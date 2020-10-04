0e00166e5c5b0b63345e7cb97f724d06c3592e10
David Ramirez <ramirezdvd@gmail.com>
2020-10-04 10:45:17 -0400

    Com

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace MathLibCircleInfo_DavidRamires
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_RadiusOfACircle = new System.Windows.Forms.TextBox();
+            this.EROCLabel = new System.Windows.Forms.Label();
+            this.button_Calculate = new System.Windows.Forms.Button();
+            this.DLabel = new System.Windows.Forms.Label();
+            this.Label_Diameter = new System.Windows.Forms.Label();
+            this.CLabel = new System.Windows.Forms.Label();
+            this.Label_Circumference = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Label_Area = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_RadiusOfACircle
+            // 
+            this.txt_RadiusOfACircle.Location = new System.Drawing.Point(11, 48);
+            this.txt_RadiusOfACircle.Name = "txt_RadiusOfACircle";
+            this.txt_RadiusOfACircle.Size = new System.Drawing.Size(100, 20);
+            this.txt_RadiusOfACircle.TabIndex = 0;
+            // 
+            // EROCLabel
+            // 
+            this.EROCLabel.Location = new System.Drawing.Point(8, 12);
+            this.EROCLabel.Name = "EROCLabel";
+            this.EROCLabel.Size = new System.Drawing.Size(100, 33);
+            this.EROCLabel.TabIndex = 1;
+            this.EROCLabel.Text = "Please enter the radius of the circle:";
+            this.EROCLabel.Click += new System.EventHandler(this.EROCLabel_Click);
+            // 
+            // button_Calculate
+            // 
+            this.button_Calculate.Location = new System.Drawing.Point(144, 45);
+            this.button_Calculate.Name = "button_Calculate";
+            this.button_Calculate.Size = new System.Drawing.Size(75, 23);
+            this.button_Calculate.TabIndex = 2;
+            this.button_Calculate.Text = "Calculate";
+            this.button_Calculate.UseVisualStyleBackColor = true;
+            this.button_Calculate.Click += new System.EventHandler(this.button_Calculate_Click);
+            // 
+            // DLabel
+            // 
+            this.DLabel.Location = new System.Drawing.Point(67, 92);
+            this.DLabel.Name = "DLabel";
+            this.DLabel.Size = new System.Drawing.Size(50, 16);
+            this.DLabel.TabIndex = 3;
+            this.DLabel.Text = "Diameter";
+            // 
+            // Label_Diameter
+            // 
+            this.Label_Diameter.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Label_Diameter.Location = new System.Drawing.Point(119, 85);
+            this.Label_Diameter.Name = "Label_Diameter";
+            this.Label_Diameter.Size = new System.Drawing.Size(100, 23);
+            this.Label_Diameter.TabIndex = 4;
+            this.Label_Diameter.Click += new System.EventHandler(this.Label_Diameter_Click);
+            // 
+            // CLabel
+            // 
+            this.CLabel.Location = new System.Drawing.Point(42, 129);
+            this.CLabel.Name = "CLabel";
+            this.CLabel.Size = new System.Drawing.Size(80, 16);
+            this.CLabel.TabIndex = 3;
+            this.CLabel.Text = "Circumference";
+            // 
+            // Label_Circumference
+            // 
+            this.Label_Circumference.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Label_Circumference.Location = new System.Drawing.Point(119, 122);
+            this.Label_Circumference.Name = "Label_Circumference";
+            this.Label_Circumference.Size = new System.Drawing.Size(100, 23);
+            this.Label_Circumference.TabIndex = 4;
+            this.Label_Circumference.Click += new System.EventHandler(this.Label_Circumference_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(85, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Area";
+            // 
+            // Label_Area
+            // 
+            this.Label_Area.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Label_Area.Location = new System.Drawing.Point(119, 159);
+            this.Label_Area.Name = "Label_Area";
+            this.Label_Area.Size = new System.Drawing.Size(100, 23);
+            this.Label_Area.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 193);
+            this.Controls.Add(this.Label_Area);
+            this.Controls.Add(this.Label_Circumference);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Label_Diameter);
+            this.Controls.Add(this.CLabel);
+            this.Controls.Add(this.DLabel);
+            this.Controls.Add(this.button_Calculate);
+            this.Controls.Add(this.EROCLabel);
+            this.Controls.Add(this.txt_RadiusOfACircle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_RadiusOfACircle;
+        private System.Windows.Forms.Label EROCLabel;
+        private System.Windows.Forms.Button button_Calculate;
+        private System.Windows.Forms.Label DLabel;
+        private System.Windows.Forms.Label Label_Diameter;
+        private System.Windows.Forms.Label CLabel;
+        private System.Windows.Forms.Label Label_Circumference;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label_Area;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathLibCircleInfo_DavidRamires
+
+//Author: David Ramirez 
+//Goal-Purpose of the Program: The purpose of the program is to calculate the area, circumference and the diameter
+//of a circle when the radius is entered into the text box.
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //  Button to calculate the radius into the given answer labels
+        private void button_Calculate_Click(object sender, EventArgs e)
+        {
+            // To convert the text into intergers.
+            double Mytxt_RadiusOfACircle = 0;
+            double MyLabel_Diameter = 0;
+            double MyLabel_Circumference = 0;
+            double MyLabel_Area = 0;
+
+            // Converts the string representation of a number to its double-precision
+            //floating-point (Floating point is used to represent fractional values) number equivalent.
+            Mytxt_RadiusOfACircle = double.Parse(txt_RadiusOfACircle.Text);
+
+            //Calculations to calculate the answers to the texbox ammount given
+            //PI represents the number 
+            MyLabel_Diameter = Mytxt_RadiusOfACircle * 2;
+            MyLabel_Circumference = 2 * (Math.PI) * Mytxt_RadiusOfACircle;
+            MyLabel_Area = (Math.PI) * (Mytxt_RadiusOfACircle) * (Mytxt_RadiusOfACircle);
+
+            // Converts the number descriptions into numerical text
+            Label_Diameter.Text = MyLabel_Diameter.ToString("n2");
+            Label_Circumference.Text = MyLabel_Circumference.ToString("n2");
+            Label_Area.Text = MyLabel_Area.ToString("n2");
+
+           
+
+
+        }
+
+        private void Label_Circumference_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EROCLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label_Diameter_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
